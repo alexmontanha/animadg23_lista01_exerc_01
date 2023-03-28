5b4760825484696ddb62bfb7aad227ca8d9718da
Alexandre de Oliveira <alexmontanha@hotmail.com>
2023-03-27 21:24:50 -0300

    Primeiro Commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String mensagemAApresentar = "Alô Mundo!";
+        System.out.println(mensagemAApresentar);
+    }
+}
